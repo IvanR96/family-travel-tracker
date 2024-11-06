c1918e064e218cdcb29ad3f2d6d23c415043aa7f
IvanR96 <irios96@hotmail.com>
2024-11-05 22:38:32 -0800

    using AS keyword as last resort

--- queries.sql
+++ queries.sql
@@ -68,7 +68,7 @@ VALUES ('English Literature'), ('Maths'), ('Physics');
 INSERT INTO enrollment (student_id, class_id ) VALUES (1, 1), (1, 2);
 INSERT INTO enrollment (student_id ,class_id) VALUES (2, 2), (2, 3);
 
--- Join --
+-- Join for enrollment and AS use as well optional --
 SELECT *
 FROM enrollment 
 JOIN student ON student.id = enrollment.student_id
